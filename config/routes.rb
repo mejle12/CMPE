e18a6cb024171eae978e0518a8be52c49260f23a
Anthony Minaise <Anthony Minaise>
2017-02-09 10:09:45 -0800

    FIRST BUILD

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'more_info', to: 'posts#more'
+end
