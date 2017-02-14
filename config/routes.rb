77ea26e724d86e980423773953179f281c1c38ec
anthonyminaise <anthonyminaise97@gmail.com>
2017-02-14 10:03:07 -0800

    HW 1 w/ Heroku
    most of the changes to make HW 1 along with capability to deploy to
    Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/homepage'
+
   resources :comments
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'welcome#homepage'
   get 'more_info', to: 'posts#more'
 end
